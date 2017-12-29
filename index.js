21e570942d5c292d67c452c5c4fdd59c3e361c24
Ygor Magalhães <emaildoygor@gmail.com>
2017-12-29 16:18:13 -0200

    Testes do angular

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
 
+    $scope.titulo = 'Listando algumas cidades';
+
     $scope.cidades;
 
-    $http({
-        method: 'GET',
-        url: 'http://ibge.herokuapp.com/municipio/?val=GO'
-    }).then((response) => {
-        $scope.cidades = response.data;
-        console.log($scope.cidades);
+    $scope.listaCidades = () => {
+        $http({
+            method: 'GET',
+            url: 'http://ibge.herokuapp.com/municipio/?val=GO'
+        }).then((response) => {
+            $scope.cidades = response.data;
+            console.log($scope.cidades);
+    
+        }, (response) => {
+            console.log(response);
+    
+        });
+    }
 
-    }, (response) => {
-        console.log(response);
-
-    });
+    $scope.listaCidades();
 });
